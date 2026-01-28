b1e1f901ad0afe1760df3aa1ab843bd61ff47a9c
shankar117512 <shankar117512@gmail.com>
2026-01-28 13:26:47 +0530

    static files added

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
